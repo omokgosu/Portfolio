33b2c87a20376cf0c69ea14015213901db18a360
양진성 <anfakt0606@naver.com>
2023-07-18 11:16:27 +0900

    reset.css , header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import '../styles/Header.css'
 
 export default function Header() {
+  
   return (
     <header className="Header">
-      
+      <h2 className="hidden">네비게이션 영역</h2>
+      <div className="headerInner">
+        <h1><a href="./index.html">양진성 포트폴리오 로고</a></h1>
+        <nav>
+          <ul className="QuickList">
+            <li>
+              About
+            </li>
+            <li>
+              Skills
+            </li>
+            <li>
+              Archiving
+            </li>
+            <li>
+              Projects
+            </li>
+            <li>
+              Career
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
