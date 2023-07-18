27ee150c67bd34fd56ecbef0d264e71ab1c0c728
양진성 <anfakt0606@naver.com>
2023-07-18 18:09:25 +0900

    main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
   
   return (
     <header className="Header">
-      <h2 className="hidden">네비게이션 영역</h2>
       <div className="headerInner">
         <h1><a href="./index.html">양진성 포트폴리오 로고</a></h1>
         <nav>
+          <h2 className="hidden">네비게이션 영역</h2>
           <ul className="QuickList">
             <li>
               About
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import '../styles/Main.css'
+
 export default function Main() {
   return (
-    <section className="Main">
-      
+    <section className="main">
+      <h2>&#45; YANG JIN SUNG &#45;<br/>WEB FRONTEND DEVELOPER</h2>
+      <p>안녕하세요!</p>
+      <p>UX 를 중요하게 생각하는 신입 프론트 개발자 양진성입니다</p>
+      <p>헬스와 고양이를 좋아합니다</p>
+      <div className="back"></div>
     </section>
   );
 }
